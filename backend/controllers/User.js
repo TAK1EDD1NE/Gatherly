fbec667053231ef090a5c16a809b143152bfcb89
takieeddine <takieeddinerehail@gmail.com>
2024-11-25 17:10:36 +0100

    add cloudinary conf

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 // import cloudinary from '../api/cloudinary.js'
 import pool from '../lib/db.js'
 import bcrypt from 'bcrypt'
-
+import cloudinary from '../api/cloudinary.js'
 
 const  generate_token= (user_id) => {
   return jwt.sign(user_id , process.env.ACCESS_TOKEN_SECRET)
@@ -10,8 +10,8 @@ const  generate_token= (user_id) => {
 
 export const signup = async (req, res, next) => {
     try{
-        const {first_name , last_name , email, pfp , password} = req.body
-
+        const {first_name , last_name , email, photo , password} = req.body
+        // checking variables
         if (!first_name || !last_name || !email ){
           res.status(400)
           throw new Error('please fill in all required fields')
@@ -20,10 +20,21 @@ export const signup = async (req, res, next) => {
           res.status(400)
           throw new Error('password should contain more than 8 characters')
         }
+        let avatar = ''
+        if (!!photo) {
+          const {secure_url: url} = await cloudinary.uploader.upload(photo, {
+            folder: "gatherly",
+            width: 200,
+            height: 200,
+            crop: 'fill',
+            gravity: 'face'
+          })
+          avatar = url          
+        }
         const hashed_password = await bcrypt.hash(password , 10)
         
         const query = 'INSERT INTO users (first_name, last_name, email, pfp, password ) VALUES ($1, $2, $3, $4, $5)'
-        const values = [ first_name, last_name , email, pfp, hashed_password]
+        const values = [ first_name, last_name , email, avatar, hashed_password]
         await pool.query(query, values)
         return res.status(201).send()
       }catch(err){
@@ -45,8 +56,6 @@ export const login = async(req, res, next) => {
       
       const token = generate_token(user.id)
       res.cookie('tigerToken', token, {
-        
-
         httpOnly: true,
         expires: new Date(Date.now() + 864e5),
         secure: true,
