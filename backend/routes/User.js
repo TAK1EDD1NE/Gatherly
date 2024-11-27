321d030266b580f60bc546dec771fd01d135d5ea
takieeddine <takieeddinerehail@gmail.com>
2024-11-27 12:45:34 +0100

    add update routes

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,7 +1,7 @@
 import express from 'express';
 // import access from '../middlewares/auth.js';
 // import ROLES from '../utils/roles.js'
-import { signup, login , signout, get_user_by_id , remove} from '../controllers/User.js';
+import { signup, login , signout, get_user_by_id , update_name,update_password,update_photo, remove} from '../controllers/User.js';
 import { auth_token } from '../middleware/auth.js';
 const userRoutes = express.Router()
 
@@ -10,4 +10,8 @@ userRoutes.post('/signup', signup)
 userRoutes.get('/signout', signout)
 userRoutes.get('/get-user-by-id/:user_id', get_user_by_id)
 userRoutes.delete('/delete', auth_token,  remove)
+userRoutes.patch('/update-name',auth_token, update_name )
+userRoutes.patch('/update-photo',auth_token, update_photo)
+userRoutes.patch('/update-password',auth_token, update_password )
+
 export default userRoutes
