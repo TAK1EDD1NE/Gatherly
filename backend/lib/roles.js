a488527bc4be9c0c7d5dae472523f408405ce4bf
takieeddine <takieeddinerehail@gmail.com>
2024-11-28 19:09:20 +0100

    add the role based access control

---
+++ backend/lib/roles.js
@@ -0,0 +1,7 @@
+const ROLES = {
+    user: ['user'],
+    admin: ['admin'],
+    employee: ['employee'],
+    all: ['user', 'admin' , 'employee']
+  }
+  export default ROLES
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,31 +1,38 @@
 import jwt from 'jsonwebtoken'
 import pool from '../lib/db.js'
-export const auth_token = async (req, res, next)=>{
-    try{        
-        const token = req.cookies.tigerToken
-            if (!token) {
+
+export const auth_token = (roles) =>{
+    return async (req, res, next)=>{
+        try{        
+            const token = req.cookies.tigerToken
+                if (!token) {
+                    res.sendStatus(401)
+                    throw new Error('session expired, please login.')
+                }
+            
+            const verified = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+            
+            if(!verified){
                 res.sendStatus(401)
-                throw new Error('session expired, please login.')
+                throw new Error('not authorized.')
             }
-        
-        const verified = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
-        
-        if(!verified){
-            res.sendStatus(401)
-            throw new Error('not authorized.')
+            const user_id = verified
+            
+            const user = (await pool.query('SELECT * FROM users WHERE id = $1', [user_id])).rows[0]
+            if(!user){
+                res.sendStatus(401)
+                throw new Error('user not found')
+            }
+            if (!roles.includes(user.role)) {
+                res.status(403)
+                throw new Error('not authorised to access this route')
+              }
+              
+            req.user = user
+            next()
+        }catch(err){
+            next(err)
         }
-        const user_id = verified
         
-        const user = (await pool.query('SELECT * FROM users WHERE id = $1', [user_id])).rows[0]
-        if(!user){
-            res.sendStatus(401)
-            throw new Error('user not found')
-        }
-        
-        req.user = user
-        next()
-    }catch(err){
-        next(err)
     }
-    
 }
