8cd25b187b4cc524af8b130723029c1a9ec521ac
takieeddine <takieeddinerehail@gmail.com>
2025-01-24 14:38:12 +0100

    changes about the cookie (contains the user not only the id)

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,19 +4,22 @@ import pool from '../lib/db.js'
 export const auth_token = (roles) =>{
     return async (req, res, next)=>{
         try{          
+
             const token = req.cookies.tigerToken
+            
             if (!token) {
                 res.sendStatus(401)
                 throw new Error('session expired, please login.')
             }
             
             const verified = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+            console.log(verified);
             
             if(!verified){
                 res.sendStatus(401)
                 throw new Error('not authorized.')
             }
-            const user_id = verified
+            const user_id = verified.id
             
             const user = (await pool.query('SELECT * FROM users WHERE id = $1', [user_id])).rows[0]
             if(!user){
